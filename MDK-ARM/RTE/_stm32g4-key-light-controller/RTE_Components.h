dd580c279d84646961ef754b1c7bb476239da4e5
username <email>
2026-04-04 22:39:25 +0800

    Created

---
+++ MDK-ARM/RTE/_stm32g4-key-light-controller/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32g4-key-light-controller' 
+ * Target:  'stm32g4-key-light-controller' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/stm32g4-key-light-controller/startup_stm32g431xx.d
@@ -0,0 +1,1 @@
+stm32g4-key-light-controller\startup_stm32g431xx.o: startup_stm32g431xx.s
